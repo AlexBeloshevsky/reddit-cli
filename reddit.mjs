ebad20d406fa153779d4ea69129e5a56dd2b1a65
Alex <alexbeloshevsky@gmail.com>
2021-08-01 16:15:30 +0300

    created a small reddit cli

---
+++ reddit.mjs
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import fetch from "node-fetch";
+import open from "open";
+import yargs from "yargs";
+
+const { argv } = yargs(process.argv);
+
+const res = await fetch("https://reddit.com/.json");
+const data = await res.json();
+const children = data.data.children;
+const randPost =
+  children[Math.floor(Math.random() * data.data.children.length)];
+const link = `https://reddit.com${randPost.data.permalink}`;
+
+if (argv.print) {
+  console.log(`
+        title: ${randPost.data.title}
+        link: ${link}
+    `);
+} else {
+  open(link);
+}
